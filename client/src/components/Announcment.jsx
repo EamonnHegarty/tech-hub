da62c0589a45f1d63627f592bcf46bd1d6af738c
Eamonn Hegarty <eamonnhegartyy@hotmail.co.uk>
2023-07-21 13:09:17 +0100

    changed color theme

--- client/src/components/Announcment.jsx
+++ client/src/components/Announcment.jsx
@@ -2,7 +2,7 @@ import { styled } from "styled-components";
 
 const Container = styled.div`
   height: 30px;
-  background-color: teal;
+  background-color: DarkCyan;
   color: white;
   display: flex;
   align-items: center;
@@ -12,7 +12,7 @@ const Container = styled.div`
 `;
 
 const Announcment = () => {
-  return <Container>Super Deal! Free Shipping on Orders Over €50</Container>;
+  return <Container>Free Shipping on Orders Over €50</Container>;
 };
 
 export default Announcment;
