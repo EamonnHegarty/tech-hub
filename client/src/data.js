4ee2cc7e2d9dddad95458368f26282e0254cbae3
Eamonn Hegarty <eamonnhegartyy@hotmail.co.uk>
2023-07-24 10:19:03 +0100

    added slider with custom transition

---
+++ client/src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "https://images.unsplash.com/photo-1616832362542-295a8648a351?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
+    title: "KEYBOARD SALE",
+    desc: "GET THE ALL NEW KEYCHRON K2 V2 FOR JUST €120.99",
+    bg: "f5fafd",
+  },
+  {
+    id: 2,
+    img: "https://images.unsplash.com/photo-1613496391962-716cf99e7e58?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1331&q=80",
+    title: "KEYCAPS SALE",
+    desc: "GET THE CHERRY PROFILE MULAN 159 DYE SUB KEYCAPS FOR JUST €49.99",
+    bg: "fcf1ed",
+  },
+  {
+    id: 3,
+    img: "https://images.unsplash.com/photo-1649194271718-c150fa081f4b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=464&q=80",
+    title: "MOUSE SALE",
+    desc: "GET THE NEW AND IMPROVED LOGITECH MX MASTER 2S FOR JUST €89.99",
+    bg: "fbf0f4",
+  },
+];
