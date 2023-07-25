fa63013d3907e064350f33dd2cb3fc1454597e41
Eamonn Hegarty <eamonnhegartyy@hotmail.co.uk>
2023-07-25 16:34:03 +0100

    added products and product

--- client/src/data.js
+++ client/src/data.js
@@ -8,17 +8,17 @@ export const sliderItems = [
   },
   {
     id: 2,
-    img: "https://images.unsplash.com/photo-1613496391962-716cf99e7e58?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1331&q=80",
-    title: "KEYCAPS SALE",
-    desc: "GET THE CHERRY PROFILE MULAN 159 DYE SUB KEYCAPS FOR JUST €49.99",
-    bg: "fcf1ed",
-  },
-  {
-    id: 3,
-    img: "https://images.unsplash.com/photo-1649194271718-c150fa081f4b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=464&q=80",
+    img: "https://images.unsplash.com/photo-1629701764404-907eed4a139d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1332&q=80",
     title: "MOUSE SALE",
     desc: "GET THE NEW AND IMPROVED LOGITECH MX MASTER 2S FOR JUST €89.99",
     bg: "fbf0f4",
+  },
+  {
+    id: 3,
+    img: "https://images.unsplash.com/photo-1552831388-6a0b3575b32a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1332&q=80",
+    title: "MONITORS SALE",
+    desc: "GET THE CHERRY PROFILE MULAN 159 DYE SUB KEYCAPS FOR JUST €49.99",
+    bg: "fcf1ed",
   },
 ];
 
@@ -37,5 +37,39 @@ export const categories = [
     id: 3,
     img: "https://images.unsplash.com/photo-1616138276663-5041438a8476?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=435&q=80",
     title: "MONITORS",
+  },
+];
+export const popularProducts = [
+  {
+    id: 1,
+    img: "https://assets3.razerzone.com/XjS5NVQdSbtfdHkfbMCfmktpeoo=/300x300/https%3A%2F%2Fhybrismediaprod.blob.core.windows.net%2Fsys-master-phoenix-images-container%2Fh4e%2Fh60%2F9533910286366%2Fblackwidow-v3-evisu-4-500x500.png",
+  },
+  {
+    id: 2,
+    img: "https://assets3.razerzone.com/OIO4GRol7t1vVcIOHQ95KdsGyLo=/500x500/https%3A%2F%2Fhybrismediaprod.blob.core.windows.net%2Fsys-master-phoenix-images-container%2Fh37%2Fhda%2F9257514041374%2F211102-pro-type-ultra-500x500.png",
+  },
+  {
+    id: 3,
+    img: "https://assets3.razerzone.com/pAYbv__1hDknbv0Jj5-Rw39CrRw=/500x500/https%3A%2F%2Fhybrismediaprod.blob.core.windows.net%2Fsys-master-phoenix-images-container%2Fh2a%2Fhd3%2F9538807103518%2Fblackwidow-v3-black-5-500x500.png",
+  },
+  {
+    id: 4,
+    img: "https://assets3.razerzone.com/pY56Nm_6T-Hcu4W14csRLXf5chw=/500x500/https%3A%2F%2Fhybrismediaprod.blob.core.windows.net%2Fsys-master-phoenix-images-container%2Fh34%2Fh51%2F9594596884510%2Fviper-8khz-2-500x500.png",
+  },
+  {
+    id: 5,
+    img: "https://assets3.razerzone.com/Y6frQWwPRq7TrhnEwNTBPkJME1M=/500x500/https%3A%2F%2Fhybrismediaprod.blob.core.windows.net%2Fsys-master-phoenix-images-container%2Fh2d%2Fhae%2F9592950194206%2Fcobra-pro-hyperpolling-dongle-500x500.png",
+  },
+  {
+    id: 6,
+    img: "https://p1-ofp.static.pub/fes/cms/2022/09/26/e5zcvdovulx2x4w0s2m77d1jiynv4e356567.png",
+  },
+  {
+    id: 7,
+    img: "https://p2-ofp.static.pub/fes/cms/2022/09/26/lrfqocd0ufldhr4phmir4u4ssnhems449743.png",
+  },
+  {
+    id: 8,
+    img: "https://assets3.razerzone.com/jqTPU3pUADwr1fK48nAYOfFoPkM=/500x500/https%3A%2F%2Fhybrismediaprod.blob.core.windows.net%2Fsys-master-phoenix-images-container%2Fh1a%2Fh53%2F9188160569374%2Fraptor-27-165hz-500x500.png",
   },
 ];
